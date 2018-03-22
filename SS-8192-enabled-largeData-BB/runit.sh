56c800f6c9dfe7556d2a068b719bcf57257cb429
Debbie Bard <djbard@gmail.com>
2018-03-22 15:19:14 -0700

    adding BB runs

---
+++ SS-8192-enabled-largeData-BB/runit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=Cosmo_job
+#SBATCH --time=00:20:00
+#SBATCH --nodes=8192
+#SBATCH --ntasks-per-node=1
+#SBATCH -L SCRATCH
+#SBATCH -C knl,quad,cache
+#SBATCH --exclusive
+#SBATCH --reservation=cosmoflow_test
+#SBATCH -A nstaff
+#DW persistentdw name=CosmoFlow
+
+
+module load /global/cscratch1/sd/djbard/cosmoML/module/March19-newCray-global
+
+unset OMP_NUM_THREADS
+export KMP_AFFINITY=compact,norespect
+export KMP_HW_SUBSET=66C@2,1T
+
+
+BZsize=1
+i=8192
+
+LOGName=LogCosmoFlow_N$(( ${i} ))BZ$(( ${BZsize} ))
+rm -rf result/*
+
+echo date
+
+LD_PRELOAD=./syfix.so srun -u -n ${i} --ntasks-per-node=1 --nodes=${i} --cpu_bind=none   python CosmoNet.py > ${LOGName} 2>&1
+
+
