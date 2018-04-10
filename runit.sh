002e5ed6cfa1e62343d73ed5b0bf8f53d699dd30
Pete Mendygral <pjm@cray.com>
2018-04-10 09:29:19 -0700

    adding in reservation info

--- runit.sh
+++ runit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=Cosmo_job
-#SBATCH --time=12:00:00
-#SBATCH --nodes=160
+#SBATCH --time=00:10:00
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH -L SCRATCH
-#SBATCH --partition=regular
 #SBATCH -C knl,quad,cache
 #SBATCH --exclusive
 #SBATCH -A dasrepo
+#SBATCH --reservation=cosmoflow
 #DW persistentdw name=CosmoFlow3param
 
 module load /global/cscratch1/sd/pjm/modulefiles/cosmoflow-gb-apr10
